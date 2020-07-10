4943efd24d8ed499d383f8a793fbd298cb9ac29b
reddevilcero <reddevil_cero@hotmail.com>
2020-07-10 20:26:18 +0100

    add spy to test

--- test/helpers.js
+++ test/helpers.js
@@ -1,8 +1,11 @@
 const chai = require("chai");
 global.expect = chai.expect;
+global.chai = chai;
 const jsdom = require("mocha-jsdom");
 const path = require("path");
 const babel = require("babel-core");
+const spies = require("chai-spies");
+chai.use(spies);
 
 const babelResult = babel.transformFileSync(
   path.resolve(__dirname, "..", "index.js"),
