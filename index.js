c709073546e169f4f5d28492fbee781b99541712
reddevilcero <reddevil_cero@hotmail.com>
2020-07-10 18:56:41 +0100

    fix error test on index instead of test folder

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
-describe("First exercise Sintax errors", () => {
-  describe("addNumbers function should return the sume of the 3 given numbers", () => {
-    it("return the correct number", () => {
-      expect(addNumbers(1, 2, 3)).to.equal(6);
-    });
-  });
+// There are syntax errors in this code - can you fix it to pass the tests?
 
-  describe("introduceMe function should return a string with the interpolation of name and age", () => {
-    it("return the correct string", () => {
-      expect(introduceMe("Sonjide", 27)).to.equal(
-        "Hello, my name is Sonjide and I am 27 years old"
-      );
-    });
-  });
+//======================= exercise 1 ==============================//
 
-  describe("getReminder function should return a string with the interpolation of the remainder", () => {
-    it("return the string with the reminder of the 2 given numbers", () => {
-      expect(getRemainder(23, 5)).to.equal("The remainder is 3");
-    });
-  });
-});
+function addNumbers(a, b, c) {
+  return a + b + c;
+}
+
+function introduceMe(name, age) {
+  return "Hello, my name is " + name + " and I am " + age + " years old";
+}
+
+function getRemainder(a, b) {
+  // declare remainder correctly
+  remainder = a %+ b;
+
+  // Use string interpolation here
+  return "The remainder is ${remainder}";
+}
--- test/index.test.js
+++ test/index.test.js
@@ -1,1 +1,22 @@
 // test here
+describe("First exercise Sintax errors", () => {
+  describe("addNumbers function should return the sume of the 3 given numbers", () => {
+    it("return the correct number", () => {
+      expect(addNumbers(1, 2, 3)).to.equal(6);
+    });
+  });
+
+  describe("introduceMe function should return a string with the interpolation of name and age", () => {
+    it("return the correct string", () => {
+      expect(introduceMe("Sonjide", 27)).to.equal(
+        "Hello, my name is Sonjide and I am 27 years old"
+      );
+    });
+  });
+
+  describe("getReminder function should return a string with the interpolation of the remainder", () => {
+    it("return the string with the reminder of the 2 given numbers", () => {
+      expect(getRemainder(23, 5)).to.equal("The remainder is 3");
+    });
+  });
+});
