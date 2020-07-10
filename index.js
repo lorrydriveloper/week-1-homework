24411b7eb36e56b7fd73d0bb39910778f716e995
reddevilcero <reddevil_cero@hotmail.com>
2020-07-10 20:26:07 +0100

    exercise 4

--- index.js
+++ index.js
@@ -49,3 +49,78 @@ function concatenate(firstWord, secondWord, thirdWord) {
   // Write the body of this function to concatenate three words together
   // Look at the test case below to understand what to expect in return
 }
+
+//======================= exercise 4 ==============================//
+
+/*
+  SALES TAX
+  =========
+  A business requires a program that calculates how much sales tax to charge
+  Sales tax is 20% of the price of the product
+*/
+
+function calculateSalesTax() {}
+
+/*
+  CURRENCY FORMATTING
+  ===================
+  The business has informed you that prices must have 2 decimal places
+  They must also start with the currency symbol
+  Write a function that transforms numbers into the format £0.00
+  Remember that the prices must include the sales tax (hint: you already wrote a function for this!)
+*/
+
+function formatCurrency() {}
+
+//======================= exercise 5 ==============================//
+
+/**
+Let's peer into the future using a Magic 8 Ball!
+https://en.wikipedia.org/wiki/Magic_8-Ball 
+There are a few steps to being able view the future though:
+* Ask a question
+* Shake the ball
+* Get an answer
+* Decide if it's positive or negative
+The question can be anything, but the answers are fixed,
+and have different levels of positivity or negativity.
+Below are the possible answers:
+## Very positive
+It is certain.
+It is decidedly so.
+Without a doubt.
+Yes - definitely.
+You may rely on it.
+## Positive
+As I see it, yes.
+Most likely.
+Outlook good.
+Yes.
+Signs point to yes.
+## Negative
+Reply hazy, try again.
+Ask again later.
+Better not tell you now.
+Cannot predict now.
+Concentrate and ask again.
+## Very negative
+Don't count on it.
+My reply is no.
+My sources say no.
+Outlook not so good.
+Very doubtful.
+*/
+
+// This should log "The ball has shaken!"
+// and return the answer.
+function shakeBall() {}
+
+// The answer should come from shaking the ball
+let answer;
+
+// When checking the answer, we should tell someone if the answer is
+// - very positive
+// - positive
+// - negative
+// - very negative
+function checkAnswer() {}
